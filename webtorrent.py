0fd7bdc8659109bb2f43a5ddaf3709ba3ded1013
digitalmadness <36610721+digitalmadness@users.noreply.github.com>
2019-05-18 21:26:16 +0900

    v2

--- webtorrent.py
+++ webtorrent.py
@@ -1,17 +1,52 @@
-import pyperclip,subprocess,time,psutil
+import pyperclip,subprocess,time,datetime,psutil,os,shutil,wx
 
-def kill(proc_pid):
-    process = psutil.Process(proc_pid)
-    for proc in process.children(recursive=True):
-        proc.kill()
-    process.kill()
 
-clipboard = pyperclip.paste()
-if 'magnet' in clipboard:
-    proc = subprocess.Popen('webtorrent '+clipboard+' --mpv',shell=True)
-    time.sleep(1)
-    subprocess.run('mpv http://localhost:8000/0')
-    '''try:
-        proc.wait(timeout=3)
-    except subprocess.TimeoutExpired:
-        kill(proc.pid)'''
+def main():
+    clipboard = pyperclip.paste()
+    if not 'magnet:' in clipboard:
+        clipboard = txtdialog()
+    magnet = clipboard.split('&')[0]
+    timestart = time.time()
+    launch(magnet)
+    seek = False
+    with open(r'C:\Users\vl70x\AppData\Roaming\mpv\log') as mpvlogfile:
+        mpvlog = mpvlogfile.readlines()
+    for x in mpvlog:
+        if 'duration' in x:
+            x = int(x.split(': ')[1].split('.')[0])
+            break
+    for y in mpvlog:
+        if 'seek to' in y:
+            seek = True
+            y = int(y.split('to ')[1].split('.')[0])
+            break
+    if seek:
+        timeleft = x-y
+    else:
+        timeleft = x-(time.time()-timestart)
+    if timeleft>x*0.1 and dialog('restart? '+str(datetime.timedelta(seconds=timeleft))+' left to watch',''):
+        launch(magnet)
+
+
+def launch(magnet):
+    return subprocess.call('webtorrent '+magnet+' --mpv',shell=True)
+
+
+def txtdialog(parent=None, message='insert magnet link'):
+    dlg = wx.TextEntryDialog(parent, message)
+    dlg.ShowModal()
+    result = dlg.GetValue()
+    dlg.Destroy()
+    return result
+
+
+def dialog(header,message):
+    dlg = wx.MessageDialog(None,header,message,wx.YES_NO | wx.ICON_WARNING)
+    result = dlg.ShowModal()
+    if result == wx.ID_YES:
+        return True
+
+
+if __name__ == '__main__':
+    app = wx.App()
+    main()
